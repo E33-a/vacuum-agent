d14476495f5e84834c80da661e049e8eeb1b272a
E33-a <emax03736@gmail.com>
2026-06-01 12:47:05 -0600

    fix: coreccion en intervalos aceptados para rows y columns (para evitar rebase de memoria)

--- Validator.h
+++ Validator.h
@@ -1,45 +1,48 @@
 #ifndef VALIDATOR_H
 #define VALIDATOR_H
 
-#include <iostream>
 #include "Utilities.h"
+#include <iostream>
 
 using namespace std;
 
 class Validator {
-    private:
-        int rowIndex, columnIndex;
-    public:
-        Validator() : rowIndex(0), columnIndex(0) {}
+private:
+  int rowIndex, columnIndex;
 
-        int getRowIndex() {
-            return rowIndex;
-        }
+public:
+  Validator() : rowIndex(0), columnIndex(0) {}
 
-        int getColumnIndex() {
-            return columnIndex;
-        }
+  int getRowIndex() { return rowIndex; }
 
-        void getData() {
-            do {
-                cout << "Enter the rows (5 or more):    ";
-                cin >> rowIndex;
-                if (rowIndex < 5) {
-                    cout << "Invalid value. Try again.\n";
-                }
-            } while (rowIndex < 5);
+  int getColumnIndex() { return columnIndex; }
 
-            do {
-                cout << "Enter the columns (5 or more):     ";
-                cin >> columnIndex;
-                if (columnIndex < 5) {
-                    cout << "Invalid value. Try again.\n";
-                }
-            } while (columnIndex < 5);
-            clearScreen();
-        }
+  void getData() {
+    do {
+      cout << "Enter the rows (5 or more):    ";
+      cin >> rowIndex;
+      if (rowIndex < 5 || rowIndex > 100) {
+        cout << "Invalid value. Try again.\n";
+        if (rowIndex > 100)
+          cout << "Please enter fewer rows; very large values may use a lot "
+                  "of memory.\n\n";
+      }
+    } while (rowIndex < 5 || rowIndex > 100);
 
-        ~Validator(){}
+    do {
+      cout << "Enter the columns (5 or more):     ";
+      cin >> columnIndex;
+      if (columnIndex < 5 || columnIndex > 100) {
+        cout << "Invalid value. Try again.\n\n";
+        if (columnIndex > 100)
+          cout << "Please enter fewer columns; very large values may use a "
+                  "lot of memory.\n";
+      }
+    } while (columnIndex < 5 || columnIndex > 100);
+    clearScreen();
+  }
+
+  ~Validator() {}
 };
 
-#endif                                                 
+#endif
